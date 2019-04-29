0a5c0c7976c3e8c059c2c835f7cbe0c0a1535ef9
yaya514 <2487261885@qq.com>
2019-04-29 19:28:11 +0800

    提交一个logging文件

---
+++ spring-boot-03-logging/src/test/java/com/itheima/springboot/SpringBoot03LoggingApplicationTests.java
@@ -0,0 +1,18 @@
+package com.itheima.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBoot03LoggingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+
+    }
+
+}
